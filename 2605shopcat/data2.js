9c03257a79c17c186af0c8f48c762c7aff04a697
manhtien <ManhTien@gmail.com>
2018-08-25 19:31:04 +0700

    no message

---
+++ 2605shopcat/data2.js
@@ -0,0 +1,337 @@
+var data = {
+	"menu": {
+		"label":"Menu",
+		"items":[
+			{
+				"link": "#",
+				"name": "About"
+			},
+			{
+				"link": "#",
+				"name": "Aparel"
+			},
+			{
+				"link": "#",
+				"name": "Windsurf"
+			},
+			{
+				"link": "#",
+				"name": "Kitesurf"
+			},
+			{
+				"link": "#",
+				"name": "Accessories"
+			},
+			{
+				"link": "#",
+				"name": "Sale"
+			},
+			{
+				"link": "#",
+				"name": "Brands"
+			},
+			{
+				"link": "#",
+				"name": "Blog"
+			},
+			{
+				"link": "#",
+				"name": "Gadgets"
+			},
+			{
+				"link": "#",
+				"name": "Contact"
+			},
+		]
+	},
+	"category_options":{
+		"label": "Category Options",
+		"items":[
+			{
+				"link": "#",
+				"name": "Men"
+			},
+			{
+				"link": "#",
+				"name": "Women"
+			},
+			{
+				"link": "#",
+				"name": "Kid"
+			},
+			{
+				"link": "#",
+				"name": "wetsuits"
+			},
+			{
+				"link": "#",
+				"name": "outerwear"
+			},
+		]
+	},
+	"brand_options":{
+		"label": "Brand Options",
+		"items":[
+			{
+				"link": "#",
+				"name": "Billabong"
+			},
+			{
+				"link": "#",
+				"name": "Element"
+			},
+			{
+				"link": "#",
+				"name": "Hermes"
+			},
+			{
+				"link": "#",
+				"name": "Chanel"
+			},
+			{
+				"link": "#",
+				"name": "Gucci"
+			},
+			{
+				"link": "#",
+				"name": "D & G"
+			},
+			{
+				"link": "#",
+				"name": "Samsung"
+			},
+			{
+				"link": "#",
+				"name": "Jacks"
+			},
+		]
+	},
+	"tags":{
+		"label" : "Tags Widget",
+		"items": [
+			{
+				"link": "#",
+				"name": "Kitesurf"
+			},
+			{
+				"link": "#",
+				"name": "Supper"
+			},
+			{
+				"link": "#",
+				"name": "Theme"
+			},
+			{
+				"link": "#",
+				"name": "Aparel"
+			},
+			{
+				"link": "#",
+				"name": "Men"
+			},
+			{
+				"link": "#",
+				"name": "Women"
+			},
+			{
+				"link": "#",
+				"name": "Gucci"
+			},
+			{
+				"link": "#",
+				"name": "Dupper"
+			},
+		]
+	},
+	"products":{
+		"currency": "USD",
+		"symbol": "$",
+		"items":[
+			{	
+				"image":"images/image1.png",
+				"link": "#",
+				"name": "Spring spot",
+				"price": 20.5
+			},
+			{	
+				"image":"images/image2.png",
+				"link": "#",
+				"name": "Salty days",
+				"price": 50
+			},
+			{	
+				"image":"images/image3.png",
+				"link": "#",
+				"name": "Cherry Jane",
+				"price": 25.75
+			},
+			{	
+				"image":"images/image4.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 120
+			},
+			{	
+				"image":"images/image5.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 315.5
+			},
+			{	
+				"image":"images/image6.png",
+				"link": "#",
+				"name": "Pluto magazee",
+				"price": 122.6
+			},
+			{	
+				"image":"images/image1.png",
+				"link": "#",
+				"name": "Spring spot",
+				"price": 20.5
+			},
+			{	
+				"image":"images/image2.png",
+				"link": "#",
+				"name": "Salty days",
+				"price": 50
+			},
+			{	
+				"image":"images/image3.png",
+				"link": "#",
+				"name": "Cherry Jane",
+				"price": 25.75
+			},
+			{	
+				"image":"images/image4.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 120
+			},
+			{	
+				"image":"images/image5.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 315.5
+			},
+			{	
+				"image":"images/image6.png",
+				"link": "#",
+				"name": "Pluto magazee",
+				"price": 122.6
+			},
+			{	
+				"image":"images/image1.png",
+				"link": "#",
+				"name": "Spring spot",
+				"price": 20.5
+			},
+			{	
+				"image":"images/image2.png",
+				"link": "#",
+				"name": "Salty days",
+				"price": 50
+			},
+			{	
+				"image":"images/image3.png",
+				"link": "#",
+				"name": "Cherry Jane",
+				"price": 25.75
+			},
+			{	
+				"image":"images/image4.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 120
+			},
+			{	
+				"image":"images/image5.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 315.5
+			},
+			{	
+				"image":"images/image6.png",
+				"link": "#",
+				"name": "Pluto magazee",
+				"price": 122.6
+			},
+			{	
+				"image":"images/image1.png",
+				"link": "#",
+				"name": "Spring spot",
+				"price": 20.5
+			},
+			{	
+				"image":"images/image2.png",
+				"link": "#",
+				"name": "Salty days",
+				"price": 50
+			},
+			{	
+				"image":"images/image3.png",
+				"link": "#",
+				"name": "Cherry Jane",
+				"price": 25.75
+			},
+			{	
+				"image":"images/image4.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 120
+			},
+			{	
+				"image":"images/image5.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 315.5
+			},
+			{	
+				"image":"images/image6.png",
+				"link": "#",
+				"name": "Pluto magazee",
+				"price": 122.6
+			},
+			{	
+				"image":"images/image1.png",
+				"link": "#",
+				"name": "Spring spot",
+				"price": 20.5
+			},
+			{	
+				"image":"images/image2.png",
+				"link": "#",
+				"name": "Salty days",
+				"price": 50
+			},
+			{	
+				"image":"images/image3.png",
+				"link": "#",
+				"name": "Cherry Jane",
+				"price": 25.75
+			},
+			{	
+				"image":"images/image4.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 120
+			},
+			{	
+				"image":"images/image5.png",
+				"link": "#",
+				"name": "Og snap",
+				"price": 315.5
+			},
+			{	
+				"image":"images/image6.png",
+				"link": "#",
+				"name": "Pluto magazee",
+				"price": 122.6
+			},
+		]	
+	}
+}
+
+
+
+
+
